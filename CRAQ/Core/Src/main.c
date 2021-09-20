133df71a6b2a0b6020b088dfbd1777ed0e2b3f15
Thomas Reichhalter <treichhalter@gmail.com>
2021-09-20 23:17:24 +0200

    Add CCS811 header file

--- CRAQ/Core/Src/main.c
+++ CRAQ/Core/Src/main.c
@@ -22,6 +22,7 @@
 #include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
+#include "ccs811.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
