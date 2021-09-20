36017af4f0c3e1e331165d0b8efce70f50131c77
Thomas Reichhalter <treichhalter@gmail.com>
2021-09-20 22:29:44 +0200

    Configuration and driver for CCS811

---
+++ CRAQ/BSP/Src/ccs811.c
@@ -0,0 +1,102 @@
+/*
+	Driver for the ccs811 sensor for stm32.
+	Copyright (C) 2019  Paulo Pereira	(EEIC-UMinho)
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file ccs811.c
+ * @brief Driver for the ccs811 sensor for stm32.
+ */
+
+#include "ccs811.h"
+#include "i2c.h"
+
+/**
+  * @brief  initialization process for the ccs811 sensor
+  * @retval None
+  */
+void ccs811_init(void)
+{
+	uint8_t startup_conf = APP_START;
+	uint8_t meas_mode_conf = 0;
+	meas_mode_conf = DRIVE_MODE(DRIVE_MODE_MODE1);
+
+	HAL_I2C_Master_Transmit(&CCS811_I2C_HANDLER, CCS811_I2C_ADDR<<1, &startup_conf, 1, 100);	//start application
+	HAL_Delay(1);
+	HAL_I2C_Mem_Write(&CCS811_I2C_HANDLER, CCS811_I2C_ADDR<<1, MEAS_MODE, I2C_MEMADD_SIZE_8BIT, &meas_mode_conf, 1, 100); //configure
+	HAL_Delay(1);
+	ccs811_temp_rh_compensation(20.0, 20.0);
+}
+
+/**
+  * @brief  read sensor i2c co2 register
+  * @retval CO2 concentration in ppb
+  */
+uint32_t ccs811_measureCO2(void)
+{
+	uint32_t co2 = 0;
+	uint32_t tvoc = 0;
+	ccs811_measure_CO2_TVOC(&co2, &tvoc);
+	return co2;
+}
+
+/**
+  * @brief  read sensor i2c tvoc register
+  * @retval TVOC concentration in ppb
+  */
+uint32_t ccs811_measureTVOC(void)
+{
+	uint32_t co2 = 0;
+	uint32_t tvoc = 0;
+	ccs811_measure_CO2_TVOC(&co2, &tvoc);
+	return tvoc;
+}
+
+/**
+  * @brief  read sensor i2c co2 and tvoc registers
+  * @param  co2		return CO2 concentration in ppb
+  * @param  tvoc	return TVOC concentration in ppb
+  * @retval None
+  */
+void ccs811_measure_CO2_TVOC(uint32_t *co2, uint32_t *tvoc)
+{
+	uint8_t alg_result[4] = {0};
+	uint32_t aux_co2;
+	uint32_t aux_tvoc;
+
+	HAL_I2C_Mem_Read(&CCS811_I2C_HANDLER, CCS811_I2C_ADDR<<1, ALG_RESULT_DATA, I2C_MEMADD_SIZE_8BIT, alg_result, 4, 100);
+
+	aux_co2 = alg_result[1] + (alg_result[0]<<8);		//co2 in ppm
+	aux_tvoc = alg_result[2] + (alg_result[3]<<8);	//tvoc in ppb
+
+	*co2 = aux_co2 * 1000;	//co2 in ppb
+	*tvoc = aux_tvoc;				//tvoc in ppb
+}
+
+/**
+  * @brief  write temperature and humidity in i2c registers for compensation
+  * @param  temp	temperature value for compensation
+  * @param  rh		humidity value for compensation
+  * @retval None
+  */
+void ccs811_temp_rh_compensation(float temp, float rh)
+{
+	uint8_t env_data[4] = {0};
+
+	env_data[0] = (( (uint32_t)(rh*512) ) & 0xFF00)>>8;		//humidity high byte
+	env_data[1] = (( (uint32_t)(rh*512) ) & 0x00FF);		//humidity low byte
+
+	env_data[2] = (( (uint32_t)(rh*512)-25 ) & 0xFF00)>>8;	//temperature high byte
+	env_data[3] = (( (uint32_t)(rh*512)-25 ) & 0x00FF);		//temperature low byte
+
+	HAL_I2C_Mem_Write(&CCS811_I2C_HANDLER, CCS811_I2C_ADDR<<1, ENV_DATA, I2C_MEMADD_SIZE_8BIT, env_data, 4, 100);
+}
---
+++ CRAQ/Debug/BSP/Src/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../BSP/Src/ccs811.c 
+
+C_DEPS += \
+./BSP/Src/ccs811.d 
+
+OBJS += \
+./BSP/Src/ccs811.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+BSP/Src/%.o: ../BSP/Src/%.c BSP/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G031xx -c -I../Core/Inc -I../BSP/Inc -I../Drivers/STM32G0xx_HAL_Driver/Inc -I../Drivers/STM32G0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G0xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
